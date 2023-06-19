ca73a1fa198cd6f34c247bada1edb90b22fcabaa
Korissa. RisakoSuzuki <okasi_aru570@icloud.com>
2023-06-19 23:46:55 +0900

    Initial Commit

---
+++ move/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  move
+//
+//  Created by 鈴木理紗子 on 2023/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
