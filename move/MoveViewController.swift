66d2f19bfa3844a5afc6e65c1398b155ff7d71b9
Korissa. RisakoSuzuki <okasi_aru570@icloud.com>
2023-06-21 14:38:55 +0900

    dekita

---
+++ move/MoveViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MoveViewController.swift
+//  move
+//
+//  Created by 鈴木理紗子 on 2023/06/20.
+//
+
+import UIKit
+
+class MoveViewController: UIViewController {
+    var saveData = ""
+    
+    @IBOutlet weak var testLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        testLabel.text = saveData
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- move/ViewController.swift
+++ move/ViewController.swift
@@ -8,15 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-   
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet var testTextField: UITextField!
+    
+    @IBAction func nextButton(){
+        let saveData = ["test": testTextField.text!]
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
+            // もし、segue(=画面間を繋ぐ矢印)のidentifierが"toDisplay"なら
+            if segue.identifier == "toDisplay" {
+
+                // 遷移先のViewController（=segueの行き先　=DisplayViewController）の宣言
+                let nextView = segue.destination as! MoveViewController
+
+                // 「値渡し」の実行。遷移先のViewController（=DisplayViewController）のargStringにこのViewContrler上のtextField1に書かれた文字を代入
+                nextView.saveData = testTextField.text!
+            }
+        }
+   
 }
 
